60b78774201334501c833f9a6eca44732c6fd276
billk97 <bill1897@yahoo.gr>
2018-11-03 22:55:11 +0200

    tests FOR FLIP added

--- CodeFolder/Tests/StateTest.java
+++ CodeFolder/Tests/StateTest.java
@@ -86,9 +86,116 @@ public class StateTest {
         g1.Predict("O");
         Assert.assertEquals(".",g1.getValue(8,1));
     }
+    @Test
+    public void predictDiagonalRightDown() {
+        State g1 = new State();
+        g1.Initializer();
+        g1.addElement(1,1,"X");
+        g1.addElement(2,2,"O");
+        g1.addElement(3,3,"O");
+        g1.addElement(6,6,"O");
+        g1.addElement(7,7,"O");
+        g1.Predict("X");
+        Assert.assertEquals(".",g1.getValue(8,8));
+    }
 
     @Test
-    public void flipElements() {
+    public void FlipLeft() {
+        State g1 = new State();
+        g1.Initializer();
+        g1.addElement(4,3,"O");
+        g1.addElement(4,2,"O");
+        g1.addElement(4,1,"X");
+        g1.FlipElements(4,1,"X");
+        Assert.assertEquals("X",g1.getValue(4,2));
+    }
+    @Test
+    public void FlipRight() {
+        State g1 = new State();
+        g1.Initializer();
+        g1.addElement(4,6,"X");
+        g1.addElement(4,7,"X");
+        g1.addElement(4,8,"O");
+        g1.FlipElements(4,8,"O");
+        Assert.assertEquals("O",g1.getValue(4,7));
+    }
+    @Test
+    public void FlipUp() {
+        State g1 = new State();
+        g1.Initializer();
+        g1.addElement(3,4,"O");
+        g1.addElement(2,4,"O");
+        g1.addElement(1,4,"X");
+        g1.FlipElements(1,4,"X");
+        Assert.assertEquals("X",g1.getValue(2,4));
+    }
+    @Test
+    public void FlipDown() {
+        State g1 = new State();
+        g1.Initializer();
+        g1.addElement(6,4,"X");
+        g1.addElement(7,4,"X");
+        g1.addElement(8,4,"O");
+        g1.FlipElements(8,4,"O");
+        Assert.assertEquals("O",g1.getValue(7,4));
+    }
+    @Test
+    public void FlipDiagonalLeftUp() {
+        State g1 = new State();
+        g1.Initializer();
+        g1.addElement(6,6,"X");
+        g1.addElement(3,3,"O");
+        g1.addElement(2,2,"O");
+        g1.addElement(1,1,"X");
+        g1.FlipElements(1,1,"X");
+        Assert.assertEquals("X",g1.getValue(2,2));
+    }
+    @Test
+    public void FlipDiagonalLeftUp2() {
+        State g1 = new State();
+        g1.Initializer();
+        g1.addElement(5,6,"X");
+        g1.addElement(6,7,"O");
+        g1.addElement(3,4,"X");
+        g1.addElement(2,3,"O");
+        g1.FlipElements(2,3,"O");
+        Assert.assertEquals("O",g1.getValue(3,4));
+    }
+    @Test
+    public void FlipDiagonalRightUp() {
+        State g1 = new State();
+        g1.Initializer();
+        g1.addElement(6,3,"O");
+        g1.addElement(3,6,"X");
+        g1.addElement(2,7,"X");
+        g1.addElement(1,8,"O");
+        g1.FlipElements(1,8,"O");
+        Assert.assertEquals("O",g1.getValue(2,7));
+    }
+    @Test
+    public void flipDiagonalLeftDown() {
+        State g1 = new State();
+        g1.Initializer();
+        g1.addElement(2,7,"O");
+        g1.addElement(3,6,"X");
+        g1.addElement(6,3,"X");
+        g1.addElement(7,2,"X");
+        g1.addElement(8,1,"O");
+        g1.FlipElements(8,1,"O");
+        Assert.assertEquals("O",g1.getValue(7,2));
+    }
+    @Test
+    public void flipDiagonalRightDown() {
+        State g1 = new State();
+        g1.Initializer();
+        g1.addElement(1,1,"X");
+        g1.addElement(2,2,"O");
+        g1.addElement(3,3,"O");
+        g1.addElement(6,6,"O");
+        g1.addElement(7,7,"O");
+        g1.addElement(8,8,"X");
+        g1.FlipElements(8,8,"X");
+        Assert.assertEquals("X",g1.getValue(7,7));
     }
 
     @Test
